cd18005552a7b4c257ef00019a284ab51f65948e
Carrick <cxia@panzura.com>
2021-01-14 17:02:24 +0800

    Fix the issue that forgetting to call flag.Parse in main.

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"github.com/golang/glog"
 	"github.com/skeyic/dnspod-ddns/app/service"
 )
 
 func main() {
+	flag.Parse()
+
 	svc := service.NewDDNSServiceFromConfig()
 	glog.V(4).Infof("DDNS Service is starting")
 	go svc.Process()
